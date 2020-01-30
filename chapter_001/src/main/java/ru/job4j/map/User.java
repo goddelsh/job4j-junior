daa80149c57e9680f6c1d2c1b770dcf320f175fb
ruslan.gadelshin <ruslan.gadelshin@paynet.uz>
2020-01-30 13:22:42 +0500

    3. Переопределить только hashCode[#222755]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -9,7 +9,16 @@ public class User {
 
     public Calendar birthday;
 
-    public User(String name) {
+
+    public User(String name, int children, Calendar birthday) {
         this.name = name;
+        this.children = children;
+        this.birthday = birthday;
     }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + children + birthday.hashCode();
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/map/UserTest.java
+++ chapter_001/src/test/java/ru/job4j/map/UserTest.java
@@ -2,19 +2,27 @@ package ru.job4j.map;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class UserTest {
 
     @Test
     public void addUserToMap() {
-        User user1 = new User("user");
-        User user2 = new User("user");
+        Calendar calendar = new GregorianCalendar(1900, 0 , 25);
+        User user1 = new User("user", 1, calendar);
+        User user2 = new User("user",1, calendar);
 
-        Map<User, Object> map = Map.of(user1, "first", user2, "second");
+        assertThat(user1.hashCode(), is(user2.hashCode()));
 
+        Map<User, Object> map = new HashMap<User, Object>();
+        map.put(user1, "first");
+        map.put(user2, "second");
         System.out.println(map);
     }
 
