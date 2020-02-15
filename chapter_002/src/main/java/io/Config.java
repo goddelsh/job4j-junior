388523010bf626027a259e49f0955efd99a47aef
goddelsh <goddelsh@gmail.com>
2020-02-15 19:45:17 +0500

    1. Читаем файл конфигурации[#222771] для статического метода корень это корневой модуль

--- chapter_002/src/main/java/io/Config.java
+++ chapter_002/src/main/java/io/Config.java
@@ -50,6 +50,6 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+        System.out.println(new Config("./chapter_002/data/app.properties"));
     }
 }
