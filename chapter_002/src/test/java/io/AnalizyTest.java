e0b4948038a50ceed2f03e29b707c2d0a8be7a61
ruslan.gadelshin <ruslan.gadelshin@paynet.uz>
2020-02-21 09:43:55 +0500

    2. Тестирование IO[#222770] тесты с TemporaryFolder

--- chapter_002/src/test/java/io/AnalizyTest.java
+++ chapter_002/src/test/java/io/AnalizyTest.java
@@ -1,22 +1,43 @@
 package io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
     @Test
-    public void unavailable() {
+    public void unavailable() throws Exception {
         Analizy analizy = new Analizy();
-        analizy.unavailable("./data/server.log", "./data/serverdown_periods.log");
-        try (BufferedReader read = new BufferedReader(new FileReader("./data/serverdown_periods.log"))) {
+        File serverLog = temporaryFolder.newFile("server.log");
+        File serverDownLog = temporaryFolder.newFile("serverdown_periods.log");
+
+
+        try (PrintStream pstrean = new PrintStream(serverLog)) {
+            var serverResponse = List.of("200 10:56:01",
+                    "",
+            "200 10:57:01",
+                    "",
+            "400 10:58:01",
+                    "",
+            "200 10:59:01",
+                    "",
+            "500 11:01:02",
+                    "",
+            "200 11:02:02");
+            serverResponse.stream().forEach(pstrean::println);
+        }
+        analizy.unavailable(serverLog.getPath(), serverDownLog.getPath());
+        try (BufferedReader read = new BufferedReader(new FileReader(serverDownLog))) {
             assertThat(read.readLine(), is("10:58:01;10:59:01;"));
             assertThat(read.readLine(), is("11:01:02;11:02:02;"));
         } catch (FileNotFoundException e) {
