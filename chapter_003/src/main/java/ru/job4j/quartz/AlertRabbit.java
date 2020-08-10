2bfe5d907e42dbc0adabc9b365b3fcc4f97f8a6e
goddelsh <goddelsh@gmail.com>
2020-08-10 22:36:20 +0500

    1. Quartz [#222713] рефактор

--- chapter_003/src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ chapter_003/src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -13,30 +13,42 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
 
-   // static Integer interval = 10;
 
-    public static void main(String[] args) {
+    static Integer getIntervalFromProps() {
+        Integer result = null;
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             Properties config = new Properties();
             config.load(in);
-            var interval = Integer.parseInt(config.getProperty("rabbit.interval"));
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            result = Integer.parseInt(config.getProperty("rabbit.interval"));
         } catch (IOException ioe) {
             ioe.printStackTrace();
         } catch (NumberFormatException nfe) {
             nfe.printStackTrace();
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            var interval = getIntervalFromProps();
+            if (interval != null) {
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDetail job = newJob(Rabbit.class).build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(interval)
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+            } else {
+                throw new IllegalStateException("interval values is null");
+            }
+        } catch (SchedulerException se) {
+            se.printStackTrace();
         }
     }
 
