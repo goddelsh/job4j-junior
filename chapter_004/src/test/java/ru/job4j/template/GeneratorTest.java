26bd79eaef6b509bef80192443d0f79bee9493f5
Ruslan <ataraveski@gmail.com>
2020-08-23 17:49:55 +0500

    1. Исправить код класс SimpleGenerator. [#222805

---
+++ chapter_004/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import java.util.Map;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class GeneratorTest {
+
+    @Ignore
+    @Test
+    public void checkWithTwoArguments() {
+        assertThat(new Generator() {
+                    @Override
+                    public String produce(String template, Map<String, String> args) {
+                        return null;
+                    }
+                }.produce("My name is ${name}. I am from ${country}",
+                Map.of("name", "Ruslan", "country",
+                        "Afghanistan")),
+                is("My name is Ruslan. I am from Afghanistan"));
+    }
+
+    @Ignore
+    @Test
+    public void checkWithIllegallArguments() {
+        var result = new Generator() {
+                    @Override
+                    public String produce(String template, Map<String, String> args) {
+                        return null;
+                    }
+                }.produce("My name is ${name}. I am from ${country}.",
+                Map.of("name", "Ruslan", "country", "${country}"));
+    }
+
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void checkWithArgumentsMoreThanInTemplate() {
+        var result = new Generator() {
+            @Override
+            public String produce(String template, Map<String, String> args) {
+                return null;
+            }
+        }.produce("My name is ${name}. I am from ${country}",
+                Map.of("name", "Ruslan", "country", "Afghanistan",  "Sex", "Male"));
+    }
+
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void checkWithArgumentsLessThanInTemplate() {
+        var result = new Generator() {
+            @Override
+            public String produce(String template, Map<String, String> args) {
+                return null;
+            }
+        }.produce("My name is ${name}. I am from ${country}",
+                Map.of("name", "Ruslan"));
+    }
+
+    @Ignore
+    @Test (expected = IllegalStateException.class)
+    public void checkWithRepeatsInTemplate() {
+        assertThat(new Generator() {
+                    @Override
+                    public String produce(String template, Map<String, String> args) {
+                        return null;
+                    }
+                }.produce("My name is ${name}. I am from ${country}. I left ${country}...${name}",
+                Map.of("name", "Ruslan", "country", "Afghanistan")),
+                is("My name is Ruslan. I am from Afghanistan. I left Afghanistan...Ruslan"));
+    }
+}
