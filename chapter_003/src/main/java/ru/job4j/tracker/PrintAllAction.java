3095f3c944d593706ceea8d0b3e4381793ef625a
Goddelsh <goddelsh@yandex.com>
2021-04-23 14:20:12 +0500

    доработка реактивного метода

--- chapter_003/src/main/java/ru/job4j/tracker/PrintAllAction.java
+++ chapter_003/src/main/java/ru/job4j/tracker/PrintAllAction.java
@@ -1,21 +1,35 @@
 package ru.job4j.tracker;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.w3c.dom.css.Counter;
+
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class PrintAllAction extends BaseAction  {
     public PrintAllAction(int key, String name) {
         super(key, name);
     }
 
+    private class Counter implements Supplier<Integer> {
+        public Integer getCount() {
+            return count;
+        }
+
+        private Integer count = 0;
+
+        @Override
+        public Integer get() {
+            return count++;
+        }
+
+
+    }
     @Override
     public boolean execute(Input input, Store tracker) {
-        final AtomicInteger aInteger = new AtomicInteger(0);
-        tracker.findAll(item -> System.out.println(aInteger.incrementAndGet() + ". id: " + item.getId() + " name: " + item.getName()));
+        Counter counter = new Counter();
+        tracker.findAll(item -> System.out.println(counter.get() + ". id: " + item.getId() + " name: " + item.getName()));
 
-        if (aInteger.get() == 0) {
+        if (counter.getCount()  == 0) {
             System.out.println("Items list is empty");
         }
         return true;
